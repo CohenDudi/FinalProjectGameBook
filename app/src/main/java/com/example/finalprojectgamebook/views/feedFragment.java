3a2a585c7e39d07be418b40f3ab16da8c6eff204
Dudi <cohdudi@gmail.com>
2021-09-04 19:47:37 +0300

    v0.1

--- app/src/main/java/com/example/finalprojectgamebook/views/feedFragment.java
+++ app/src/main/java/com/example/finalprojectgamebook/views/feedFragment.java
@@ -1,7 +1,36 @@
 package com.example.finalprojectgamebook.views;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.finalprojectgamebook.R;
+import com.example.finalprojectgamebook.viewmodel.feedViewModel;
 
 public class feedFragment extends Fragment {
+    private feedViewModel feedViewModel;
 
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        feedViewModel = new ViewModelProvider(this).get(feedViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_feed, container, false);
+        final TextView textView = root.findViewById(R.id.feed_text);
+
+        feedViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
 }
